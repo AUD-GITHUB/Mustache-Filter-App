3b38556d53697ffe7ad52ff8f2c03868ba2145a9
AUD-GITHUB <85228103+AUD-GITHUB@users.noreply.github.com>
2023-06-09 14:55:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,31 +1,35 @@
 function preload() {
-
 }
+let video;
+let poseNet;
+let poses;
 
 function setup() {
     canvas = createCanvas(400, 300);
     canvas.center();
     video = createCapture(VIDEO);
-    video.hide();
+    video.size(400,300);
+    video.hide(); 
+    webcam = video;
 
     poseNet = ml5.poseNet(video, modelLoaded());
-    poseNet.on('pose', gotPoses);
+    poseNet.on('pose', function(results) {
+        poses = results;
+        if (poses.length > 0) {
+            console.log(poses);
+            console.log('nose X = ' + poses[0].pose.nose.x);
+            console.log('nose Y = ' + poses[0].pose.nose.y);
+        }
+    })
 }
 
 function modelLoaded() {
     console.log('PoseNet is Loaded')
 }
 
-function gotPoses(results) {
-    if (results.length > 0) {
-        console.log(results);
-        console.log('nose X = ' + results[0].pose.nose.x);
-        console.log('nose Y = ' + results[0].pose.nose.y);
-    }
-}
-
 function draw() {
-    image(video, 0, 0, 400, 300);
+    image(webcam, 0, 0, 400, 300);
+
 }
 
 function take_snapshot() {
