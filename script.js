28ae9a20ea87e789d37c0380683a51698edf3aca
AUD-GITHUB <85228103+AUD-GITHUB@users.noreply.github.com>
2023-06-09 14:24:00 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,33 @@
-function preload(){
+function preload() {
 
 }
 
-function setup(){
+function setup() {
     canvas = createCanvas(400, 300);
     canvas.center();
+    video = createCapture(VIDEO);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded());
+    poseNet.on('pose', gotPoses);
 }
 
-function draw(){
-
+function modelLoaded() {
+    console.log('PoseNet is Loaded')
 }
 
-function take_snapshot(){
+function gotPoses(results) {
+    if (results.length > 0) {
+        console.log(results);
+        console.log('nose X = ' + results[0].pose.nose.x);
+        console.log('nose Y = ' + results[0].pose.nose.y);
+    }
+}
+
+function draw() {
+    image(video, 0, 0, 400, 300);
+}
+
+function take_snapshot() {
     save("Mustache_Photo.png");
 }
